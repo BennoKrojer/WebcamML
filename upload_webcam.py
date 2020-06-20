14c2eee68585d2fe6671631ee6f3b2a8b56563b9
CONATH <christorfer@me.com>
2020-06-20 18:01:40 +0200

    Add Python script that uploads webcam image

---
+++ upload_webcam.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import json
+from time import localtime, strftime
+
+time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+json.dump({'string': time}, open('date.json', 'w'))
+url = 'http://localhost:8765/picture/4/current/'  # URL from raspberry
+r = requests.get(url)
+img_data = r.content
+with open('current.jpg', 'wb') as handler:
+    handler.write(img_data)
+    
+os.system('scp current.jpg webcamml@webcamml.uber.space:/home/webcamml/html/images/current.jpg')
+os.system('scp date.json webcamml@webcamml.uber.space:/home/webcamml/WebcamML/prototype/src/assets/date.json')
