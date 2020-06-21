2ecc681a54fa2a4d78839c42a8f2385816999255
Benno Krojer <benno.krojer@gmail.com>
2020-06-21 21:55:53 +0200

    Add meta tags including title and auto refresh every three minutes

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {ReactTitle} from 'react-meta-tags';
+import MetaTags from 'react-meta-tags';
 import './App.css';
 
 var Date = require('./assets/date.json');
@@ -7,6 +9,8 @@ var RelPath = "images/current.jpg";
 function App() {
   return (
     <div className="App">
+      <meta http-equiv="refresh" content="180"/>
+      <ReactTitle title="View of Munich, Germany"/>
       <header className="App-header">
         <img width="1194" height="672" src={RelPath} className="webcam-image" alt="View of Munich, Germany including the Frauenkirche, Staatsoper, Theatinerkirche and Olympiapark." />
         <p>
