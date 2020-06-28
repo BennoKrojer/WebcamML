a9a1553dfbeec47ac578520c0646b84103ec22b5
Benno Krojer <benno.krojer@gmail.com>
2020-06-28 17:36:53 +0200

    uploads distance json as well now

--- upload_webcam.py
+++ upload_webcam.py
@@ -4,6 +4,7 @@ import json
 from time import localtime, strftime
 
 time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+json.dumo({'distance': '10'}, open('distance.json', 'w')) 
 json.dump({'string': time}, open('date.json', 'w'))
 url = 'http://localhost:8765/picture/4/current/'  # URL from raspberry
 r = requests.get(url)
@@ -11,5 +12,7 @@ img_data = r.content
 with open('current.jpg', 'wb') as handler:
     handler.write(img_data)
     
+
 os.system('scp current.jpg webcamml@webcamml.uber.space:/home/webcamml/html/images/current.jpg')
+os.system('scp distance.json webcamml@webcamml.uber.space:/home/webcamml/WebcamML/prototype/src/assets/distance.json')
 os.system('scp date.json webcamml@webcamml.uber.space:/home/webcamml/WebcamML/prototype/src/assets/date.json')
