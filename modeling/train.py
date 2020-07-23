001019124140dc3347b892a51d9fadcac5bf2bc3
CONATH <christorfer@me.com>
2020-07-23 14:00:20 +0200

    Change to classification; introduce dropout; train test split

--- modeling/train.py
+++ modeling/train.py
@@ -1,17 +1,12 @@
 from modeling.dataloader import get_image_paths
 import tensorflow as tf
+import numpy
 
-df = get_image_paths()
-
-# image data generator takes our images and applies data augmentation
-# this makes the image data more suitable as input for the neural network
-# it also generates more training data so we get more reliable results
-image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-
-
+# Parameters
 BATCH_SIZE = 32
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
+RESERVE_VALIDATION = 0.2
 # Shift and rotation is somewhat likely in our case (webcam might be moved or rotated slightly by accident)
 # So we use small value as it's not common to happen
 SHIFT_DELTA = 50
@@ -21,33 +16,50 @@ BRIGHTNESS_FACTOR_MIN = 0.1
 # zoom_range [x.x, 1.0]
 
 
+df = get_image_paths()
+
+# image data generator takes our images and applies data augmentation
+# this makes the image data more suitable as input for the neural network
+# it also generates more training data so we get more reliable results
+image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255, validation_split=0.2)
+
+numpy.random.seed(41)
+
 train_data_gen = image_generator.flow_from_dataframe(df, x_col='path', y_col='label',
                                                      batch_size=BATCH_SIZE,
                                                      shuffle=True,
                                                      class_mode='raw',
-                                                     width_shift_range=[-SHIFT_DELTA,SHIFT_DELTA],
+                                                     width_shift_range=[-SHIFT_DELTA, SHIFT_DELTA],
                                                      rotation_range=ROTATION_DELTA,
-                                                     brightness_range=[BRIGHTNESS_FACTOR_MIN,1.0],
-                                                     target_size=(IMG_HEIGHT, IMG_WIDTH))
+                                                     brightness_range=[BRIGHTNESS_FACTOR_MIN, 1.0],
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH), subset='training')
+
+valid_data_gen = image_generator.flow_from_dataframe(df, x_col='path', y_col='label',
+                                                     batch_size=BATCH_SIZE,
+                                                     shuffle=True,
+                                                     class_mode='raw',
+                                                     width_shift_range=[-SHIFT_DELTA, SHIFT_DELTA],
+                                                     rotation_range=ROTATION_DELTA,
+                                                     brightness_range=[BRIGHTNESS_FACTOR_MIN, 1.0],
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH), subset='validation')
 
 base_model = tf.keras.applications.MobileNetV2(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
                                                include_top=False,
                                                weights='imagenet')
-
 base_model.trainable = False
+
 print(base_model.summary())
 model = tf.keras.Sequential([
     base_model,
     tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(5)
 ])
-
-
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
-model.compile(loss='mse',
+model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
-              metrics=['mae', 'mse'])
+              metrics=['accuracy', 'categorical_crossentropy'])
 
-model.fit(train_data_gen, epochs=100)
+model.fit(train_data_gen, epochs=100, validation_data=valid_data_gen)
